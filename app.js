9aca9736ec5b9aa92a31b7ae99937ca921237df5
little-Dave <17536424+little-Dave@users.noreply.github.com>
2020-07-14 22:13:18 -0600

    accomplish basic functionality within app.js

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
+const path = require('path');
+
+const bodyParser = require('body-parser');
 const express = require('express');
 
 const app = express();
 
+const users = [];
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.get("/", (request, response, next) => {
-  response.send("<h1>Hello, world</h1>");
+  response.render("form");
+})
+
+app.post("/", (request, response, next) => {
+  users.push(request.body.name);
+  response.redirect("/");
+})
+
+app.get("/users", (request, response, next) => {
+  response.send(users.join(", "));
 })
 
 app.listen(3000 || process.env.PORT);
